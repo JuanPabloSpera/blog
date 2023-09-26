b233f8034a5fcd8027f035cb1ce7303fa3274f8f
juans <juanspera@student.ie.edu>
2023-09-27 01:38:41 +0200

    heroku_requirements

--- blog/settings.py
+++ blog/settings.py
@@ -140,7 +140,8 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
-STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
