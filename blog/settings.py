f08e7d428ebeeff97a15d6a8991c26b1942b4d69
juans <juanspera@student.ie.edu>
2023-09-27 20:45:32 +0200

    staticfiles dirs

--- blog/settings.py
+++ blog/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
-# STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'static'),)
+#  STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'static'),)
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
