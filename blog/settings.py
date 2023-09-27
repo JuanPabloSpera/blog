722a466c618e4acb346e21ca96ec4ffb69116b37
juans <juanspera@student.ie.edu>
2023-09-27 02:16:35 +0200

    heroku_middleware

--- blog/settings.py
+++ blog/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,11 +138,18 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
+
 DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = BASE_DIR / "static"
+
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 # STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
